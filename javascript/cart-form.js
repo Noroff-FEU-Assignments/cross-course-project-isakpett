ac42bf8fd3d2bf9ce9dc84b4eb9ce742a1e88df7
isakpett <isak_pettersen@hotmail.com>
2023-03-18 14:07:39 +0100

    jhsdl

--- javascript/cart-form.js
+++ javascript/cart-form.js
@@ -16,3 +16,38 @@ function handleBillingClick(event) {
 }
 
 billingButton.addEventListener("click", handleBillingClick);
+
+const quantityInputs = document.querySelectorAll('input[type="number"]');
+const removeButtons = document.querySelectorAll(".icon-remove");
+const subtotalCell = document.querySelector(".subtotal-cell");
+const taxCell = document.querySelector(".tax-cell");
+const totalCell = document.querySelector(".total-cell");
+const priceCells = document.querySelectorAll("td[data-price]");
+
+function updatePrices() {
+  let subtotal = 0;
+  let tax = 0;
+  let total = 0;
+
+  for (let i = 0; i < quantityInputs.length; i++) {
+    const quantity = parseInt(quantityInputs[i].value);
+    const price = parseInt(priceCells[i].dataset.price);
+    const itemTotal = quantity * price;
+    subtotal += itemTotal;
+    priceCells[i].textContent = "$" + itemTotal;
+  }
+
+  tax = subtotal * 0.08;
+  total = subtotal + tax;
+
+  subtotalCell.textContent = "$" + subtotal.toFixed(2);
+  taxCell.textContent = "$" + tax.toFixed(2);
+  totalCell.textContent = "$" + total.toFixed(2);
+}
+
+for (let i = 0; i < quantityInputs.length; i++) {
+  quantityInputs[i].addEventListener("input", updatePrices);
+  removeButtons[i].addEventListener("click", updatePrices);
+}
+
+updatePrices();
