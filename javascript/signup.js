1d46777e2d2bdabcf5fa8e42dbd930a71b273d4d
isakpett <isak_pettersen@hotmail.com>
2023-03-19 20:43:02 +0100

    xcxc

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,13 +1,15 @@
 const form = document.getElementById("signup-form");
 const message = document.getElementById("signup-message");
+const loader = document.getElementById("loader");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const email = document.getElementById("email").value;
-
+  loader.style.display = "block";
   setTimeout(() => {
-    message.textContent = `Thank you for signing up! A discount code has been sent to ${email}.`;
+    loader.style.display = "none";
+    message.innerHTML = `<p>Thank you for signing up! A discount code has been sent to ${email}.</p>`;
   }, 2000);
 
   document.getElementById("email").value = "";
